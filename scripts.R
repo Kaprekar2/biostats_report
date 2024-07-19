9ce9a2038f3cac159ff8d1dc4e472fbf26eb712c
Riki TAKARA <rikinine97@gmail.com>
2024-07-19 17:13:56 +0900

    added graph

--- scripts.R
+++ scripts.R
@@ -5,27 +5,32 @@ barley |> head()
 
 ##### 箱ひげ図 ------
 
-barley_change <- 
-  barley |> 
-  mutate(yearcolor = 
-           case_when(year == 1932 ~ "#2D579A",
-                     year == 1931 ~ "#BEC7D7"))
+#barley_change <- 
+#  barley |> 
+#  mutate(yearcolor = 
+#           case_when(year == 1932 ~ "#D59B0A",
+#                     year == 1931 ~ "#DECA98"))
 
-
-ggplot(barley_change, aes(x= variety, y=yield))+
+#varietyのと収量の関係
+ggplot(barley, aes(x= variety, y=yield))+
   theme_bw()+
   geom_boxplot()+
-  geom_dotplot(aes(fill=yearcolor), binaxis = "y", binwidth = 1,stackdir = "center")+
-  scale_fill_manual(values = c("#2D579A", "#BEC7D7"), guide = "none")+
-  xlab("yield [bu/ac]") + 
-  ylab("<span style='font-size:16pt'>variety</span>") +
-  ggtitle("<span style='font-size:20pt'>**yield graph**</span><br>") + 
-  theme(plot.title = element_text(size = 20, hjust = 0.5),
+  #geom_dotplot(aes(fill=year), binaxis = "y", binwidth = 1,stackdir = "center")+
+  #geom_point(aes(colour=year))+
+  geom_jitter(aes(colour=year),width = 0.2)+
+  scale_colour_manual(values = c("#D59B0A", "#DECA98"))+
+  labs(
+    title = "yield graph",
+    x = "variety",
+    y = "yield [bu/ac]"
+  )+
+  theme(plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
         axis.title.x = element_text(size = 16),
         axis.title.y = element_text(size = 16),
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12),
         legend.text = element_text(size = 10),
-        legend.title = element_text(size = 12)
-  )
+        legend.title = element_text(size = 12),
+        plot.caption = element_text(size = 8)
+  )+
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))
+
 
