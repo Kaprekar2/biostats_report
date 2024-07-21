b2ae8cce003fcd410f8b68809524a3aac9441e8e
Riki TAKARA <rikinine97@gmail.com>
2024-07-22 02:21:33 +0900

    added some models

--- scripts.R
+++ scripts.R
@@ -133,3 +133,83 @@ sleepstudy %>%
   nest(data = -Subject) %>%
   mutate(fit = map(data, ~ lm(Reaction ~ Days, data = .)),
          results = map(fit, ~HSD.test(., trt = 'Days')))
+
+
+##### モデル ------
+
+model1 <- function(dat)
+  lm(yield ~ variety + 1, data = dat)
+
+##栽培地(と誤差頂+切片)だけを含んだモデル
+model2 <- function(dat)
+  lm(yield ~ site + 1, data = dat)
+
+##年度(と誤差頂+切片)だけを含んだモデル
+model3 <- function(dat)
+  lm(yield ~ year + 1, data = dat)
+
+#年度の変動が品種や栽培地の違いに影響すると考えられる
+##栽培地と品種のランダム効果だけを導入したモデル
+model4 <- function(dat)
+  lmer(yield ~   1 + (1 | site) + (1|variety) , data = dat)
+
+##栽培地と切片に、年度(環境変動)と品種のランダム効果を導入したモデル
+model5 <- function(dat)
+  lmer(yield ~  site + 1 + (site + 1 | year) + (1 |variety) , data = barley)
+
+##品種には栽培地の効果がありそうだと考えられる
+##品種に年度(環境変動)と栽培地のランダム効果を導入したモデル
+model6 <- function(dat)
+  lmer(yield ~ variety + 1 + (variety + 1 | year) + (variety + 1|site) , data = dat)
+
+##栽培地と年度の相互作用のモデルに、品種のランダム効果を加えたもの
+model7 <- function(dat)
+  lmer(yield ~ site + year + site:year + (1|variety) , data = dat)
+
+##品種と年度の相互作用のモデルに、品種のランダム効果を加えたもの
+model8 <- #function(dat)
+  lmer(yield ~ variety + year + variety:year + (variety + 1|site) , data = barley)
+
+#既存のモデル
+#nullモデル
+yield_nullmodel <- function(dat)
+  lm(yield ~ 1 + (1|site) + (1|variety) + (1|year), data = dat)
+
+#栽培地と品種と栽培地と年度の相互作用を係数として織り込んだモデル
+yield_altmodel <- function(dat)
+  lm(yield ~ site + variety + 1 + site:year, data = dat)
+
+#栽培地と品種と栽培地と品種の相互作用を係数として織り込んだモデル
+variety_year_inter_model <- function(dat)
+  lm(yield ~ site + variety + 1 + variety:year, data = dat)
+
+#栽培地と年度の相互作用と品種と年度の相互作用を係数として織り込んだモデル
+all_inter_model <- function(dat)
+  lm(yield ~ site + variety + 1 + site:year + variety:year, data = dat)
+
+pacman::p_lod(cAIC4)
+
+listed_lmer <- 
+  list( model4, model5, model6, model7, model8)
+
+model_results <- barley %>% 
+  nest %>% 
+  bind_rows(., ., ., ., .) %>%          #データをモデル数分bind
+  rownames_to_column("id_model") %>% 
+  mutate(model = map2(data, listed_lmer, ~.y(.x))) %>%    # それぞれ当てはめ
+  mutate(AIC = map(model, AIC), # AIC抽出
+         fixef = map(model, fixef),                       # 固定効果
+         ranef = map(model, ranef),                       # ランダム効果
+         predict = map2(model, data, ~predict(.x, .y))) 
+
+
+listed_lmer <- 
+  list( yield_nullmodel, yield_altmodel, variety_year_inter_model, all_inter_model,
+        model1, model2, model3, model4, model5, model6, model7, model8)
+
+getAICloglik <- function(model) {
+  print(AIC(model))
+  print(logLik(model))
+}
+
+aics <- map_dbl(listed_lmer, getAICloglik)
